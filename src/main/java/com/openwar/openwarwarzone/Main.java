97a544fb962ab34bee5a45263a1b8b651ff624f3
Charpy <76815325+Charpyy@users.noreply.github.com>
2024-10-24 14:40:48 +0200

    Update Main.java

--- src/main/java/com/openwar/openwarwarzone/Main.java
+++ src/main/java/com/openwar/openwarwarzone/Main.java
@@ -2,6 +2,7 @@ package com.openwar.openwarwarzone;
 
 import com.openwar.openwarfaction.factions.FactionManager;
 import com.openwar.openwarlevels.level.PlayerDataManager;
+import com.openwar.openwarwarzone.Handler.AllowedCommands;
 import com.openwar.openwarwarzone.Handler.Crate;
 import com.openwar.openwarwarzone.Handler.LootCrate;
 import org.bukkit.plugin.RegisteredServiceProvider;
@@ -29,6 +30,7 @@ public final class Main extends JavaPlugin {
         System.out.println(" OpenWar - Warzone loading...");
         if (!setupDepend()) {return;}
         getServer().getPluginManager().registerEvents(new LootCrate(pl, this), this);
+        getServer().getPluginManager().registerEvents(new AllowedCommands(), this);
 
     }
 
