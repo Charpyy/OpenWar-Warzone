2a5f76907e219f1a8d8c18a00a54dccc93b30720
Charpy <76815325+Charpyy@users.noreply.github.com>
2024-12-11 20:49:18 +0100

    Update Main.java

--- src/main/java/com/openwar/openwarwarzone/Main.java
+++ src/main/java/com/openwar/openwarwarzone/Main.java
@@ -3,25 +3,28 @@ package com.openwar.openwarwarzone;
 import com.openwar.openwarfaction.factions.FactionManager;
 import com.openwar.openwarlevels.level.PlayerDataManager;
 import com.openwar.openwarwarzone.Handler.AllowedCommands;
-import com.openwar.openwarwarzone.Handler.Crate;
 import com.openwar.openwarwarzone.Handler.LootCrate;
 import com.openwar.openwarwarzone.WarzoneCTF.CTFHandler;
-import de.tr7zw.nbtapi.NBT;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
     private PlayerDataManager pl;
     private FactionManager fm;
+    private Economy economy = null;
+
     private boolean setupDepend() {
         RegisteredServiceProvider<PlayerDataManager> levelProvider = getServer().getServicesManager().getRegistration(PlayerDataManager.class);
         RegisteredServiceProvider<FactionManager> factionDataProvider = getServer().getServicesManager().getRegistration(FactionManager.class);
-        if (    levelProvider == null || factionDataProvider == null) {
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (    levelProvider == null || factionDataProvider == null || rsp == null) {
             System.out.println("ERROR !!!!!!!!!!!!!!!!!!!!");
             return false;
         }
         pl = levelProvider.getProvider();
         fm = factionDataProvider.getProvider();
+        economy = rsp.getProvider();
         return true;
     }
 
@@ -31,7 +34,7 @@ public final class Main extends JavaPlugin {
         System.out.println(" ");
         System.out.println(" OpenWar - Warzone loading...");
         if (!setupDepend()) {return;}
-        getServer().getPluginManager().registerEvents(new CTFHandler(fm, this), this);
+        getServer().getPluginManager().registerEvents(new CTFHandler(fm, this, economy), this);
         getServer().getPluginManager().registerEvents(new LootCrate(pl, this), this);
         getServer().getPluginManager().registerEvents(new AllowedCommands(), this);
     }
